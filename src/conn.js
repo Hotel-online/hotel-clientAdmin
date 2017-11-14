2561e3f4d253398b3453cd090350749c0bf04994
RamirezJosue <xdemobtkx@gmail.com>
2017-11-14 08:58:37 -0500

    first commit

---
+++ src/conn.js
@@ -0,0 +1,11 @@
+const config = {
+    url: "http://localhost:8003/o/authorize/",
+    client: "5FqX5b8g87ngWqqW8bImiVHo81Y6MjNqRlN9Zjoh",
+    redirect: "http://localhost:3001",
+    scope: "read",
+    width: 400, // Width (in pixels) of login popup window. Optional, default: 400
+    height: 400 // Height (in pixels) of login popup window. Optional, default: 400
+  }
+
+  export default config;
+  
